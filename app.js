411a45e6e0df315da4da04183c23f9c831012fde
Ericson <castasuse@gmail.com>
2021-10-01 14:27:48 +0800

    Finish quiz game app

--- app.js
+++ app.js
@@ -1,30 +1,62 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const _ = require("lodash");
 const app = express();
+const mongoose = require("mongoose");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-let chosenCategory = "";
+//connect to our database
+mongoose.connect("mongodb://localhost:27017/quizGameDB");
+
+const playerSchema = new mongoose.Schema({
+  name: String,
+  score: Number,
+  category: String
+});
+const Player = mongoose.model("Player", playerSchema);
+
+const samplePlayer = new Player({
+  name: "Rick",
+  score: 3,
+  category: "animals"
+});
+
+// samplePlayer.save();
+
+let category = "";
 let choices = [];
 let score = 0;
 let questionIndex = 0;
-
 let correctAnswer = "";
+let item = {};
+let listOfCorrectAnswers = [];
+let playerName = "";
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
+  category = "";
+  choices = [];
+  score = 0;
+  questionIndex = 0;
+  correctAnswer = "";
+  item = {};
+  listOfCorrectAnswers = [];
+
+  res.render("index");
 });
 
 app.get("/question/:category", function(req, res){
   //questions: https://wehavekids.com/education/Multiple-Choice-Quiz-How-well-do-you-know-animals-suitable-for-kids
-
-  chosenCategory = req.params.category;
-  fs.readFile(chosenCategory+".txt", function(err, data){
-    let item = JSON.parse(data);
-    const listOfCorrectAnswers = [];
+  category = req.params.category;
+  if(_.isEmpty(item)){
+    fs.readFile(category+".txt", function(err, data){
+      item = JSON.parse(data);
+      res.redirect("/question/"+category);
+    });
+  } else {
     if(questionIndex < item.results.length){
       let questionNumber = item.results[questionIndex].questionNumber;
       let question = item.results[questionIndex].question;
@@ -35,10 +67,28 @@ app.get("/question/:category", function(req, res){
 
       res.render("mainGame", {questionNumber: questionNumber, question: question, choices: choices});
     } else{
-      console.log(score);
-      console.log(listOfCorrectAnswers);
+
+      const player = new Player({
+        name: playerName,
+        score: score,
+        category: category
+      });
+      player.save();
+      res.render("result", {score: score, answers: listOfCorrectAnswers});
     }
+  }
+});
+app.get("/leaderboardCategory", function(req, res){
+  res.render("leaderboardCategory");
+});
+app.get("/leaderboard/:category", function(req, res){
+  Player.find({category: req.params.category}, null, {sort: {score: -1}}, function(err, players){
+    res.render("mainLeaderboard", {players: players});
   });
+});
+app.post("/", function(req, res){
+  playerName = req.body.playerName;
+  res.render("categories");
 });
 app.post("/answer", function(req, res){
   const answer = req.body.checkbox;
@@ -47,7 +97,7 @@ app.post("/answer", function(req, res){
     score+=1;
   }
   questionIndex+=1;
-  res.redirect("/question/"+chosenCategory);
+  res.redirect("/question/" + category);
 });
 
 app.listen(3000, function(){
